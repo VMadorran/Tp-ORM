7188f625da0ac624d64818657fb5c7d2399ed029
VMadorran <89205164+VMadorran@users.noreply.github.com>
2023-09-27 18:21:01 -0300

    27/9

--- tp3-ORM/src/main/java/ar/unrn/tp/api/DescuentoService.java
+++ tp3-ORM/src/main/java/ar/unrn/tp/api/DescuentoService.java
@@ -8,16 +8,11 @@ import jakarta.persistence.EntityManagerFactory;
 
 public interface DescuentoService {
 
-	// validar que las fechas no se superpongan
-	// Promocion bancaria
-
 	void DescuentoService(EntityManagerFactory emf);
 
 	void crearDescuentoSobreTotal(String marcaTarjeta, LocalDateTime fechaDesde, LocalDateTime fechaHasta,
 			float porcentaje);
 
-	// validar que las fechas no se superpongan
-	// Promocion de marca
 	void crearDescuento(String marcaProducto, LocalDateTime fechaDesde, LocalDateTime fechaHasta, float porcentaje);
 
 	public List<Promocion> listarDescuentos();
