175473c022f629521622864d744940b918f53706
VMadorran <89205164+VMadorran@users.noreply.github.com>
2023-09-27 18:16:30 -0300

    .

---
+++ tp3-ORM/src/main/java/ar/unrn/tp/modelo/ClaveVenta.java
@@ -0,0 +1,13 @@
+package ar.unrn.tp.modelo;
+
+public class ClaveVenta {
+
+	private int numero;
+	private int anio;
+
+	public ClaveVenta(int numero, int anio) {
+		this.numero = numero;
+		this.anio = anio;
+	}
+
+}
